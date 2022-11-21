daee4c1df7e38243feb6d02f2d5d2ad458909219
Martin <martin@email.com>
2022-11-21 13:52:58 +0100

    presentation page

--- HackatOrga/Accueil.Designer.cs
+++ HackatOrga/Accueil.Designer.cs
@@ -28,11 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Accueil));
             this.label1 = new System.Windows.Forms.Label();
             this.lbl_accueil = new System.Windows.Forms.Label();
             this.btn_conferences = new System.Windows.Forms.Button();
             this.btn_hackathons = new System.Windows.Forms.Button();
             this.btn_atelier = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -47,6 +50,7 @@
             // lbl_accueil
             // 
             this.lbl_accueil.AutoSize = true;
+            this.lbl_accueil.BackColor = System.Drawing.Color.CornflowerBlue;
             this.lbl_accueil.Font = new System.Drawing.Font("Amiri Quran", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.lbl_accueil.Location = new System.Drawing.Point(402, 99);
             this.lbl_accueil.Name = "lbl_accueil";
@@ -57,24 +61,26 @@
             // 
             // btn_conferences
             // 
+            this.btn_conferences.BackColor = System.Drawing.Color.CornflowerBlue;
             this.btn_conferences.Font = new System.Drawing.Font("DejaVu Sans Condensed", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.btn_conferences.Location = new System.Drawing.Point(696, 289);
             this.btn_conferences.Name = "btn_conferences";
             this.btn_conferences.Size = new System.Drawing.Size(246, 78);
             this.btn_conferences.TabIndex = 3;
             this.btn_conferences.Text = "Accéder aux conférences";
-            this.btn_conferences.UseVisualStyleBackColor = true;
+            this.btn_conferences.UseVisualStyleBackColor = false;
             this.btn_conferences.Click += new System.EventHandler(this.btn_conferences_Click);
             // 
             // btn_hackathons
             // 
+            this.btn_hackathons.BackColor = System.Drawing.Color.CornflowerBlue;
             this.btn_hackathons.Font = new System.Drawing.Font("DejaVu Sans Condensed", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.btn_hackathons.Location = new System.Drawing.Point(131, 289);
             this.btn_hackathons.Name = "btn_hackathons";
             this.btn_hackathons.Size = new System.Drawing.Size(246, 78);
             this.btn_hackathons.TabIndex = 4;
             this.btn_hackathons.Text = "Accéder aux hackathons";
-            this.btn_hackathons.UseVisualStyleBackColor = true;
+            this.btn_hackathons.UseVisualStyleBackColor = false;
             this.btn_hackathons.Click += new System.EventHandler(this.btn_hackathons_Click);
             // 
             // btn_atelier
@@ -92,6 +98,15 @@
             this.btn_atelier.UseVisualStyleBackColor = false;
             this.btn_atelier.Click += new System.EventHandler(this.btn_atelier_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1044, 624);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
             // Accueil
             // 
             this.ClientSize = new System.Drawing.Size(1040, 622);
@@ -99,7 +114,10 @@
             this.Controls.Add(this.btn_hackathons);
             this.Controls.Add(this.btn_conferences);
             this.Controls.Add(this.lbl_accueil);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Accueil";
+            this.Text = "Accueil";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -112,5 +130,6 @@
         private Button btn_conferences;
         private Button btn_hackathons;
         private Button btn_atelier;
+        private PictureBox pictureBox1;
     }
 }
--- HackatOrga/Conference.Designer.cs
+++ HackatOrga/Conference.Designer.cs
@@ -28,27 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Conference));
             this.btn_accueil = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btn_accueil
             // 
+            this.btn_accueil.BackColor = System.Drawing.Color.CornflowerBlue;
             this.btn_accueil.Location = new System.Drawing.Point(0, 0);
             this.btn_accueil.Name = "btn_accueil";
-            this.btn_accueil.Size = new System.Drawing.Size(75, 23);
+            this.btn_accueil.Size = new System.Drawing.Size(96, 35);
             this.btn_accueil.TabIndex = 0;
             this.btn_accueil.Text = "Accueil";
-            this.btn_accueil.UseVisualStyleBackColor = true;
+            this.btn_accueil.UseVisualStyleBackColor = false;
             this.btn_accueil.Click += new System.EventHandler(this.btn_accueil_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-39, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1136, 602);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
             // 
             // Conference
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1028, 585);
             this.Controls.Add(this.btn_accueil);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("DejaVu Sans Condensed", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Conference";
             this.Text = "Conference";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -56,5 +74,6 @@
         #endregion
 
         private Button btn_accueil;
+        private PictureBox pictureBox1;
     }
 }
